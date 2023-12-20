7bcf8ad24f53db82b419f1e1dcea973993755a86
traceover <47640630+traceover@users.noreply.github.com>
2023-12-19 21:52:09 -0600

    First work on a proper lexer

---
+++ src/lexer.rs
@@ -0,0 +1,323 @@
+use std::fmt;
+use std::path::PathBuf;
+use num_bigint::BigInt;
+
+/// A custom location type to represent the location of tokens or errors.
+/// This enum has two variants: `File` and `Str`.
+/// `File` is used when the source is from a file and includes the file path, row, and column.
+/// `Str` is used for string inputs and includes the column and the line as a vector of characters.
+#[derive(Debug, Clone)]
+pub enum Loc {
+    File {
+        path: String,
+        row: usize,
+        col: usize,
+    },
+    Str {
+        col: usize,
+        line: Vec<char>,
+    }
+}
+
+/// Formats the location as it would appear in many other programming language compilers,
+/// with platform-specific formatting to print backslashes on Windows.
+/// Special handling for the `Str` variant is not handled here to print a proper backtrace
+/// that will underline the erroneous code, instead, the row will be omitted and only a
+/// column number will be displayed.
+impl fmt::Display for Loc {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Loc::File { path, row, col } => {
+                write!(f, "{path}:{row}:{col}", path = PathBuf::from(&path).display(), col = col + 1)
+            },
+            Loc::Str { col, line: _ } => {
+                write!(f, "(from a string):{col}", col = col + 1)
+            },
+        }
+    }
+}
+
+pub struct Lexer {
+    chars: Vec<char>,
+    file_path: Option<String>,
+    lnum: usize,
+    bol: usize,
+    cnum: usize,
+    error: Option<LexError>,
+}
+
+impl Lexer {
+    pub fn new(chars: Vec<char>, file_path: Option<String>) -> Self {
+        Self {
+            chars,
+            file_path,
+            lnum: 0,
+            bol: 0,
+            cnum: 0,
+            error: None,
+        }
+    }
+
+    pub fn current_line(&self) -> Vec<char> {
+        let mut eol = self.bol;
+        while eol < self.chars.len() && self.chars[eol] != '\n' {
+            eol += 1;
+        }
+        return self.chars[self.bol..eol].to_vec();
+    }
+
+    pub fn loc(&self) -> Loc {
+        match &self.file_path {
+            Some(file_path) => Loc::File {
+                path: file_path.clone(),
+                row: self.lnum + 1,
+                col: self.cnum - self.bol,
+            },
+            None => Loc::Str {
+                col: self.cnum - self.bol,
+                line: self.current_line(),
+            },
+        }
+    }
+
+    #[inline]
+    fn drop_char_if<F>(&mut self, predicate: F) -> Option<char>
+    where
+         F: FnOnce(char) -> bool,
+    {
+        self.chars.get(self.cnum).copied().and_then(|ch| {
+            if predicate(ch) {
+                self.drop_char()
+            } else {
+                None
+            }
+        })
+    }
+
+    #[inline]
+    fn drop_char_by(&mut self, set: &str) -> Option<char> {
+        self.drop_char_if(|ch| set.contains(ch))
+    }
+
+    #[inline]
+    fn drop_char(&mut self) -> Option<char> {
+        self.chars.get(self.cnum).copied().map(|ch| {
+            self.cnum += 1;
+            if ch == '\n' {
+                self.bol = self.cnum;
+                self.lnum += 1;
+            }
+            ch
+        })
+    }
+
+    #[inline]
+    fn drop_line(&mut self) {
+        while let Some(x) = self.drop_char() {
+            if x == '\n' {
+                return;
+            }
+        }
+    }
+
+    #[inline]
+    fn switch2(&mut self, tok0: Token, tok1: Token) -> Token {
+        if self.drop_char_if(|ch| ch == '=').is_some() {
+            tok1
+        } else {
+            tok0
+        }
+    }
+
+    #[inline]
+    fn switch3(&mut self, tok0: Token, tok1: Token, ch: char, tok2: Token) -> Token {
+        match self.drop_char_if(|x| x == '=' || x == ch) {
+            Some('=') => tok1,
+            Some(_) => tok2,
+            None => tok0,
+        }
+    }
+
+    #[inline]
+    fn trim_whitespace(&mut self) {
+        while let Some(x) = self.chars.get(self.cnum) {
+            if x.is_whitespace() {
+                self.drop_char();
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn chop_tokens_from_chars(&mut self) -> Option<Token> {
+        'again: loop {
+            self.trim_whitespace();
+
+            use Token::*;
+            return Some(match self.drop_char()? {
+                '(' => OpenParen,
+                ')' => CloseParen,
+                '{' => OpenBrace,
+                '}' => CloseBrace,
+                '[' => OpenBracket,
+                ']' => CloseBracket,
+                ',' => Comma,
+                '.' => Period,
+                ':' => Colon,
+                ';' => Semicolon,
+                '=' => self.switch2(Equals, EqualsEquals),
+                '!' => self.switch2(Bang, BangEquals),
+                '+' => self.switch2(Plus, PlusEquals),
+                '-' => self.switch2(Minus, MinusEquals),
+                '*' => self.switch2(Asterisk, AsteriskEquals),
+                '/' => match self.drop_char_by("=/") {
+                    Some('=') => SlashEquals,
+                    Some('/') => {
+                        self.drop_line();
+                        continue 'again;
+                    },
+                    _ => Slash,
+                },
+                '%' => self.switch2(Percent, PercentEquals),
+                '&' => self.switch3(Ampersand, AmpersandEquals, '&', AmpersandAmpersand),
+                '|' => self.switch3(Pipe, PipeEquals, '|', PipePipe),
+                '^' => self.switch2(Caret, CaretEquals),
+                '"' => {
+                    let start = self.loc();
+                    let mut text = String::new();
+                    while let Some(x) = self.drop_char_if(|x| x != '"') {
+                        text.push(x);
+                    }
+                    if let Some(_) = self.drop_char_if(|x| x == '"') {
+                        StrLit(text)
+                    } else {
+                        self.error = Some(LexError::UnterminatedStrLit {
+                            start,
+                        });
+                        return None;
+                    }
+                },
+                x => {
+                    let loc = self.loc();
+                    if is_ident_char(x) {
+                        let mut text = x.to_string();
+                        while let Some(x) = self.drop_char_if(is_ident_char) {
+                            text.push(x);
+                        }
+
+                        if let Some(kind) = keyword_by_name(&text) {
+                            kind
+                        } else if text.starts_with(|c: char| c.is_ascii_digit()) {
+                            match text.parse::<BigInt>() {
+                                Ok(x) => IntLit(x),
+                                Err(_) => {
+                                    self.error = Some(LexError::InvalidIntLit {
+                                        text,
+                                        loc,
+                                    });
+                                    return None;
+                                },
+                            }
+                        } else {
+                            Ident(text)
+                        }
+                    } else {
+                        self.error = Some(LexError::InvalidChar {
+                            ch: x,
+                            loc,
+                        });
+                        return None;
+                    }
+                },
+            });
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Token {
+    // Literals / multi-char tokens
+    Ident(String),
+    StrLit(String),
+    IntLit(BigInt),
+    FloatLit(f64),
+
+    // Punctuation tokens
+    OpenParen,
+    CloseParen,
+    OpenBrace,
+    CloseBrace,
+    OpenBracket,
+    CloseBracket,
+    Comma,
+    Period,
+    Colon,
+    Semicolon,
+
+    // Operator tokens
+    Equals,
+    EqualsEquals,
+    Bang,
+    BangEquals,
+    Plus,
+    PlusEquals,
+    Minus,
+    MinusEquals,
+    Asterisk,
+    AsteriskEquals,
+    Slash,
+    SlashEquals,
+    Percent,
+    PercentEquals,
+    Ampersand,
+    AmpersandAmpersand,
+    AmpersandEquals,
+    Pipe,
+    PipePipe,
+    PipeEquals,
+    Caret,
+    CaretEquals,
+
+    // Keyword tokens
+    If,
+    Else,
+    Then,
+}
+
+pub enum LexError {
+    UnterminatedStrLit {
+        start: Loc,
+    },
+    InvalidChar {
+        loc: Loc,
+        ch: char,
+    },
+    InvalidIntLit {
+        loc: Loc,
+        text: String,
+    },
+}
+
+impl Iterator for Lexer {
+    type Item = (Token, Loc);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.trim_whitespace(); // So that the location is correct
+        let loc = self.loc();
+        Some((self.chop_tokens_from_chars()?, loc))
+    }
+}
+
+fn is_ident_char(x: char) -> bool {
+    let extra_chars = "_";
+    x.is_alphanumeric() || extra_chars.contains(x)
+}
+
+fn keyword_by_name(s: &str) -> Option<Token> {
+    match s {
+        "if" => Some(Token::If),
+        "else" => Some(Token::Else),
+        "then" => Some(Token::Then),
+        _ => None,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,73 +4,71 @@ pub mod infer;
 pub mod program;
 pub mod types;
 pub mod bytecode;
+pub mod lexer;
 
 use ast::*;
 use infer::Infer;
 use bytecode::{Inst, Opcode, Interp};
 use program::ProgramContext;
 use types::BuiltinType;
+use lexer::{Token, Lexer};
 
+use std::fs;
+use std::env;
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::process::exit;
+use std::path::Path;
 
 fn main() {
-    let mut ast = Ast::new();
-
-    let a = ast.append(Node::Number(1.into()));
-    let b = ast.append(Node::Number(2.into()));
-    let c = ast.append(Node::Number(3.into()));
-
-    let a_b = ast.append(Node::Binary(Binary::new(Oper::Add, a, b)));
-    let a_b_c = ast.append(Node::Binary(Binary::new(Oper::Add, a_b, c)));
-
-    ast.declare(Decl::with_expr("ITEM".into(), a_b_c));
-
-    let lhs = ast.append(Node::Ident("ITEM".into()));
-    let rhs = ast.append(Node::Number(2.into()));
-    let bin = ast.append(Node::Binary(Binary::new(Oper::Mul, lhs, rhs)));
-
-    let int_ty = ast.append(Node::BuiltinType(BuiltinType::Float));
-    let proto = ast.append(Node::FnProto(FnProto::new(Vec::new(), int_ty)));
-    let ret = ast.append(Node::Return(Some(bin)));
-    let block = ast.append(Node::Block(Block::new(vec![ret])));
-    let fun = ast.append(Node::FnLiteral(FnLiteral::new(proto, block)));
-    ast.declare(Decl::with_expr("fizz".into(), fun));
-
-    for decl in &ast.decls {
-        println!("{} :: {}", &decl.name, ast.display(decl.root_expr));
-    }
-    println!();
-
     let program_context = Rc::new(RefCell::new(ProgramContext::new()));
-    program_context.borrow_mut().insert_ast(&ast);
-
-    let infer = match Infer::infer(program_context, &ast) {
-        Ok(infer) => infer,
-        Err(err) => {
-            eprintln!("ERROR: {err:?}");
-            std::process::exit(1);
-        }
-    };
 
     let mut interp = Interp::new(Vec::new());
     interp.program.push(Inst::new(Opcode::Call, 0, 0, 2));
     interp.program.push(Inst::new(Opcode::Exit, 0, 0, 0));
-    interp.add_decls(&infer);
 
-    for (name, label) in &interp.labels {
-        println!("{} = {:?}", name, label);
-    }
-    println!();
+    for file_path in env::args().skip(1) {
+        let contents = fs::read_to_string(&file_path).expect(&format!("Failed to read file: {file_path}"));
+        let mut ast = Ast::new();
+        let mut node = ast.append(Node::Number(0.into()));
 
-    for inst in &interp.program {
-        println!("{inst}");
+        for (token, loc) in Lexer::new(contents.chars().collect(), Some(file_path.clone())) {
+            println!("{loc}: {token:?}");
+            match token {
+                Token::IntLit(x) => {
+                    let n = ast.append(Node::Number(x));
+                    node = ast.append(Node::Binary(Binary::new(Oper::Add, node, n)));
+                },
+                _ => {},
+            }
+        }
+
+        let int_ty = ast.append(Node::BuiltinType(BuiltinType::Int));
+        let proto = ast.append(Node::FnProto(FnProto::new(Vec::new(), int_ty)));
+        let ret = ast.append(Node::Return(Some(node)));
+        let block = ast.append(Node::Block(Block::new(vec![ret])));
+        let fun = ast.append(Node::FnLiteral(FnLiteral::new(proto, block)));
+        ast.declare(Decl::with_expr(get_file_name(Path::new(&file_path)).expect("Invalid file path"), fun));
+
+        program_context.borrow_mut().insert_ast(&ast);
+
+        let infer = match Infer::infer(program_context.clone(), &ast) {
+            Ok(infer) => infer,
+            Err(err) => {
+                eprintln!("ERROR: {err:?}");
+                exit(1);
+            }
+        };
+        interp.add_decls(&infer);
     }
-    println!();
 
     interp.run();
 
     for (reg, value) in &interp.registers {
         println!("{} = {}", reg, value);
     }
+}
+
+fn get_file_name(path: &Path) -> Option<String> {
+    path.file_stem().map(|path| path.to_string_lossy().to_string())
 }
